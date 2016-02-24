55945a60495644a0a09d8363eda0c06fa877da57
NukeZ <curtis.jah@gmail.com>
2016-02-23 20:31:27 -0800

    accidentally stayed on master, finished creating table schema with the correct username, password, and column type / name outputs

--- client/components/App.js
+++ client/components/App.js
@@ -7,24 +7,66 @@ var App = React.createClass({
 
 
   getInitialState: function() {
-    return { columns: 0, table: '', un: '', pw: '', string: '' }
+    // "schema":{
+    // "database": {
+    //   "type"     : 'sql',
+    //   "username" : 'Sequelize.String',
+    //   "password" : 'Sequelize.String',
+    //   "url"      : 'localhost://3000'
+    // },
+    //
+    // "data": {
+    //   "table": 'table_name',
+    //   "colORdocs" : [{
+    //     "username" : "johnCrackersmacker",
+    //     "password" : "yabbadabbado",
+    //     "url" : "www.super.com"
+    //     }]
+    //   }
+    // }
+    var state = {
+      columns: 0,
+      database: {
+        type: 'sql',
+        username: 'Sequelize.String',
+        password: 'Sequelize.String',
+        url : 'localhost://3000'
+      },
+      data: {
+        table: '',
+        colORdocs: []
+      }
+    };
+
+    return state;
   },
 
   makeTableSchema: function(e) {
+    console.log(e);
     e.preventDefault();
     // this.setState({columns: $('#numberOfColumns').val(), table: $('#TableInput').val(), un: $('#UsernameOfDatabase').val(), pw: $('#PasswordOfDatabase').val()});
     // this.sendInfoToForm
-    this.state.sqlSchema = [];
+
+    // this.state.sqlSchema = [];
+    this.state.data.colORdocs = [];
     var currentState = this.state;
     for (var i = 0; i < this.state.columns; i++) {
-      var name = document.getElementById('colnames' + i).value;
-      var type = document.getElementById('coltype' + i).value;
-      console.log(name);
-      console.log(type);
-      if (name === '') return alert('Add name to column ' + (i + 1));
-      currentState.sqlSchema.push('Name:' + name + 'Type: ' + type);
+      var col = {};
+      col.name = document.getElementById('colnames' + i).value;
+      col.type = document.getElementById('coltype' + i).value;
+      // console.log(name);
+      // console.log(type);
+      if (col.name === '') return alert('Add name to column ' + (i + 1));
+      currentState.data.colORdocs.push(col);
     }
-    console.log('currentState.sqlSchema: ', currentState.sqlSchema);
+    currentState.data.table = document.getElementById('tableName').value;
+    currentState.database.username = document.getElementById('usernameOfDb').value;
+    currentState.database.password = document.getElementById('passwordOfDb').value;
+    // col.name = name;
+    // col.type = type
+    // console.log('currentState.sqlSchema: ', currentState.sqlSchema);
+    console.log('currentState.data.colORdocs: ', currentState.data.colORdocs);
+    console.log('currentState ', currentState);
     this.state.columns = 0;
     this.setState(currentState);
   },
